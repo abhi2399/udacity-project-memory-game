41a299d14a407fa7a9d8b727690bc54b93401aae
Abhishek Dadhich <abhishek.11612455@lpu.in>
2018-08-11 17:50:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,4 @@
-/*
- * Create a list that holds all of your cards
- */
 
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
-// Shuffle function from http://stackoverflow.com/a/2450976
 const icons = ["fa fa-diamond", "fa fa-diamond", "fa fa-paper-plane-o", "fa fa-paper-plane-o", "fa fa-anchor", "fa fa-anchor", "fa fa-bolt", "fa fa-bolt", "fa fa-cube", "fa fa-cube", "fa fa-leaf", "fa fa-leaf", "fa fa-bicycle", "fa fa-bicycle", "fa fa-bomb", "fa fa-bomb"]; 
 
 
@@ -69,40 +57,49 @@ const card=document.createElement("li");
 card.classList.add("card");
 card.innerHTML = "<i class= '" + icons[i] + "'</i>";
 cardsContainer.appendChild(card);
+
+// add click event to each card	
 click(card);  
  }
 }
- 
+// click event
 function click(card) {
+//card click event
 card.addEventListener("click", function (){
 const currentCard = this;
 const previousCard = openedCards[0];
 
-    
+// we have an existing opened card
 if(openedCards.length === 1){ 
 card.classList.add("open", "show", "disable");    
 openedCards.push(this);     
-          
+	
+// we should compare our two opened card
 if(this.innerHTML === openedCards[0].innerHTML) {
 currentCard.classList.add("match");
 previousCard.classList.add("match"); 
   
+//match
 matchedCards.push(currentCard, previousCard);          
 openedCards = []; 
+
+//check if the game is over
 isOver();
   
   
 } else {
-  
+
+// wait 500ms, then do this
 setTimeout(function(){
 currentCard.classList.remove ("open","show", "disable");
 previousCard.classList.remove ("open","show", "disable"); openedCards = []; 
   }, 500);
- 
+
+//new move
 addMove(); 
  
 }  
-  
+// we don't have opened card  
 } else {
   
 currentCard.classList.add("open", "show", "disable");    
@@ -122,16 +119,17 @@ alert("GAME OVER! YOU Won! Do You Want to Play Again? Your Stars are " +  countS
  clearInterval(timerVar); 
 }
 }
-
+//add move
 const movesContainer = document.querySelector(".moves");
 let moves = 0;
 movesContainer.innerHTML = 0
 function addMove() {
  moves++;
   movesContainer.innerHTML = moves;
-  
+ //set the rating 
 rating();
 }
+//rating
 const starsContainer = document.querySelector(".stars");
 starsContainer.innerHTML = `<li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li>
         		            <li><i class="fa fa-star"></i></li>`;
@@ -148,14 +146,14 @@ function rating() {
         		                    <li><i class="fa fa-star"></i></li>`;
   }
 }
-
+//restart button
 const restartBtn = document.querySelector(".restart");
 restartBtn.addEventListener("click", function(){
- 
+// delete all cards 
  cardsContainer.innerHTML = " "; 
-  
+  // call init() to create new cards
   init();
-  
+  //reset any relatable variables
 matchedCards=[];
 moves = 0;
 movesContainer.innerHTML = moves; 
@@ -163,7 +161,7 @@ starsContainer.innerHTML = `<li><i class="fa fa-star"></i></li><li><i class="fa 
         		                <li><i class="fa fa-star"></i></li>`;
 
 });
-
+//start the game fisrt time
 init();
 
 function shuffle(array) {
